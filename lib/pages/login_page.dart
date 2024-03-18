eb9caa4f00c1279dc7b7ccc4aabcc49c191291b8
Alif T <alift929@gmail.com>
2024-03-18 15:20:27 +0700

    add screen up when keyboard appears

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:oktm_service_mobile/widgets/custom_text_field.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -6,109 +7,87 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      body: ListView(
+        physics: const BouncingScrollPhysics(),
         children: [
-          Container(
-            padding: const EdgeInsets.only(top: 50, bottom: 50),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Column(
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: const EdgeInsets.only(top: 50, bottom: 50),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Image.asset('assets/images/logo_telu_login.png', width: 150, height: 150),
-                    const Text(
-                      'OKTM Service Mobile',
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.w600,
-                          color: Color(0xFF9E0000),
-                        )
-                      ),
-                    const Text(
-                      'Telkom University',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w600,
-                          color: Color(0xFF9E0000),
-                        )
+                    Column(
+                      children: [
+                        Image.asset('assets/images/logo_telu_login.png', width: 150, height: 150),
+                        const Text(
+                          'OKTM Service Mobile',
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.w600,
+                              color: Color(0xFF9E0000),
+                            )
+                          ),
+                        const Text(
+                          'Telkom University',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600,
+                              color: Color(0xFF9E0000),
+                            )
+                        ),
+                      ],
                     ),
                   ],
                 ),
-              ],
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  'SSO Login',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.w600,
-                    )
-                ),
-                const SizedBox(height: 50),
-                TextField(
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Color(0xFFEBEBEB),
-                    hintText: 'Username',
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(color: Color(0xFFCECECE), width: 0.5),
-                      borderRadius: BorderRadius.circular(10),
+              ),
+              Container(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'SSO Login',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.w600,
+                        )
                     ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(color: Color(0xFFCECECE), width: 0.5),
-                      borderRadius: BorderRadius.circular(10),
-                    )
-                  ),
-                ),
-                const SizedBox(height: 25),
-                TextField(
-                  obscureText: true,
-                  enableSuggestions: false,
-                  autocorrect: false,
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Color(0xFFEBEBEB),
-                    hintText: 'Password',
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(color: Color(0xFFCECECE), width: 0.5),
-                      borderRadius: BorderRadius.circular(10),
+                    const SizedBox(height: 50),
+                    const CustomTextField(
+                      hintText: 'Username',
                     ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(color: Color(0xFFCECECE), width: 0.5),
-                      borderRadius: BorderRadius.circular(10),
-                    )
-                  ),
-                ),
-                const SizedBox(height: 50),
-                ElevatedButton(
-                  onPressed: (){},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xFF9E0000),
-                    minimumSize: Size(double.infinity, 50),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
+                    const SizedBox(height: 25),
+                    const CustomTextField(
+                      hintText: 'Password',
+                      isPasswordInput: true,
                     ),
-                  ),
-                  child: const Text(
-                    'Login',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.white
+                    const SizedBox(height: 50),
+                    ElevatedButton(
+                      onPressed: (){},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFF9E0000),
+                        minimumSize: Size(double.infinity, 50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      child: const Text(
+                        'Login',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white
+                        )
+                      ),
                     )
-                  ),
+                  ],
                 )
-              ],
-            )
+              ),
+              // SizedBox(height:MediaQuery.of(context).viewInsets.bottom+20),
+            ],
           ),
-          SizedBox(height:MediaQuery.of(context).viewInsets.bottom+20),
         ],
       )
     );
