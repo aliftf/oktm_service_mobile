3ca34aab6da1aeab5afa999cafe948b8dbce5d6a
Gapherionζ <iqrobanyuanto12345@gmail.com>
2024-03-19 21:14:01 +0700

    add UI layout & desain

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -11,43 +11,100 @@ class ProfilePage extends StatelessWidget {
         child: Stack(
           children: [
             Container(
-              height: MediaQuery.of(context).size.height / 2,
-              color: Colors.red,
+              height: MediaQuery.of(context).size.height - 550,
+              color: const Color(0xFF9E0000),
             ),
             Container(
-              height: MediaQuery.of(context).size.height - 300,
-              width: MediaQuery.of(context).size.width,
+              alignment: Alignment.center,
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Container(
+                    padding: const EdgeInsets.only(bottom: 30),
                     child: Container(
-                      height: MediaQuery.of(context).size.height - 500,
-                      width: MediaQuery.of(context).size.width - 50,
+                      height: MediaQuery.of(context).size.height - 550,
+                      width: MediaQuery.of(context).size.width -65,
                       decoration: BoxDecoration(
                         color: Colors.white,
                         shape: BoxShape.rectangle,
-                        borderRadius: BorderRadius.circular(10),
+                        borderRadius: BorderRadius.circular(20),
                         border: Border.all(color: Colors.black)
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Column(
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(bottom: 14),
+                                child: Container(
+                                  height: 170,
+                                  width: 170,
+                                  decoration: const BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    image: DecorationImage(
+                                      image: AssetImage('assets/images/user_icon.png'),
+                                      fit: BoxFit.contain,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              const Text(
+                                "Mahasiswa",
+                                style: TextStyle(
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.bold,
+                                )
+                              )
+                            ],
+                          ),
+                          const Column(
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.only(bottom: 8),
+                                child: Text(
+                                  "1002210001",
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                  )
+                                ),
+                              ),
+                              Text(
+                                "Mahasiswa",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                )
+                              )
+                            ],
+                          )
+                        ],
                       ),
                     ),
                   ),
                   ElevatedButton(
                     onPressed: () {
-                      // Add your button onPressed logic here
+                      // Logic untuk button
                     },
                     style: ButtonStyle(
                       fixedSize: MaterialStateProperty.all<Size>(
-                        Size(MediaQuery.of(context).size.width - 50, 50),
+                        Size(MediaQuery.of(context).size.width - 65, 73),
                       ),
-                      backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+                      backgroundColor: MaterialStateProperty.all<Color>(const Color(0xFF9E0000)),
                       shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                         RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10.0),
+                          borderRadius: BorderRadius.circular(25),
                         ),
                       ),
                     ),
-                    child: const Text('Edit Profil', style: TextStyle(color: Colors.white),),
+                    child: const Text(
+                      'Logout',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
+                    ),
                   )
                 ],
               ),
@@ -56,6 +113,8 @@ class ProfilePage extends StatelessWidget {
         )
       ),
       bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: const Color(0xFF9E0000),
+        backgroundColor: const Color.fromARGB(255, 250, 250, 250),
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
@@ -66,7 +125,7 @@ class ProfilePage extends StatelessWidget {
             label:  'Riwayat',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.person),
+            icon: Icon(Icons.person_2_outlined),
             label: 'Profil',
           ),
         ],
