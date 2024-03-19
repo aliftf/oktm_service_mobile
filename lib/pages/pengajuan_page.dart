a78951f07d7173a115b18a16d29cc7c3130492b3
raimess11 <rahma.sakti24@gmail.com>
2024-03-20 01:14:57 +0700

    layout page

--- lib/pages/pengajuan_page.dart
+++ lib/pages/pengajuan_page.dart
@@ -1,4 +1,5 @@
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -20,16 +21,136 @@ class Screen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Stack(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Container(
-            child: Text("string", style: TextStyle(
-            fontSize: 16, 
-            color: Colors.white,
-            fontWeight: FontWeight.w600
-            ),),
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.all(24),
+            height: 105,
+            child: Row(
+              children: [
+                Icon(Icons.arrow_back, color: Colors.white,),
+                SizedBox(width: 10,),
+                Text("string", style: Theme.of(context).textTheme.bodyLarge,),
+              ],
+            ),
           ),
-          Container()
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(24),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(20))
+              ),
+              child: Column(
+                children: [
+                  //header s&k
+                  Row(
+                    children: [
+                      Icon(Icons.info_outline),
+                      SizedBox(width: 10,),
+                      Text("string", style: TextStyle(),),
+                    ],
+                  ),
+                  //content s&k
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    child: Text("""Ketentuan dapat mengajukan perbaikan KTM
+\u2022 Terdaftar sebagai mahasiswa yang masih menempuh pendidikan di Telkom University.
+\u2022 KTM yang ingin diperbaiki masih ada secara fisik.
+
+Persyaratan lampiran
+\u2022 KSM
+\u2022 Foto dari KTM
+\u2022 Bukti pembayaran"""
+                    ),
+                  ),
+                  //header form
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    child: Row(
+                    children: [
+                      Text("Filing Form", style: TextStyle(),),
+                    ],
+                  )),
+                  //form1
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    child: Stack(
+                      alignment: Alignment.bottomCenter,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            color:  Color(0xFF9E0000),
+                            borderRadius: BorderRadius.all(Radius.circular(20))
+                          ),
+                          height: 111,
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            color:  Colors.white,
+                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                            border: Border.all(width: 1.0, color: Color(0xFFCECECE))
+                          ),
+                          height: 91,
+                        )
+                      ]
+                    ) 
+                  ),
+                  //form2
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    child: Stack(
+                      alignment: Alignment.bottomCenter,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            color:  Color(0xFF9E0000),
+                            borderRadius: BorderRadius.all(Radius.circular(20))
+                          ),
+                          height: 111,
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            color:  Colors.white,
+                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                            border: Border.all(width: 1.0, color: Color(0xFFCECECE))
+                          ),
+                          height: 91,
+                        )
+                      ]
+                    ) 
+                  ),
+                  //form3
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    child: Stack(
+                      alignment: Alignment.bottomCenter,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            color:  Color(0xFF9E0000),
+                            borderRadius: BorderRadius.all(Radius.circular(20))
+                          ),
+                          height: 111,
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            color:  Colors.white,
+                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                            border: Border.all(width: 1.0, color: Color(0xFFCECECE))
+                          ),
+                          height: 91,
+                        )
+                      ]
+                    ) 
+                  ),
+                ],
+              )
+            ),
+          )
+          
         ],
       ),
     );
