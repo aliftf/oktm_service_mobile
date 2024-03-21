74a534e73f37173c0c05d43a2e26b89dae20ddc1
Deva Angela Felisitas <devaguava22@gmail.com>
2024-03-21 20:49:07 +0700

    first commit

---
+++ lib/pages/informasi_hasil_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InformasiHasilPage extends StatefulWidget {
+  const InformasiHasilPage({super.key});
+
+  @override
+  State<InformasiHasilPage> createState() => _InformasiHasilPageState();
+}
+
+class _InformasiHasilPageState extends State<InformasiHasilPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
